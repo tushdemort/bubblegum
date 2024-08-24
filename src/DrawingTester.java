66d41919e9576b1a8254f4ad812f818b00d31dc4
raghu3003 <shuklakshitij3003@gmail.com>
2024-08-24 10:45:46 +0530

    Dialog box of input of length and breadth

--- src/DrawingTester.java
+++ src/DrawingTester.java
@@ -14,8 +14,23 @@ public class DrawingTester {
 		circleButton.addActionListener(new ActionListener(){
 			@Override	
             public void actionPerformed(ActionEvent e){
-                    panel.addShape(new Shape(100, 150, w/2, h/3, 159, 69, 219,"newroom"));
-                    panel.addedShape();
+                    String lengthString = JOptionPane.showInputDialog(f,"Enter the length of box:");
+                    String breadthString = JOptionPane.showInputDialog(f,"Enter the breadth of box");
+                    if(lengthString!=null && breadthString!=null){
+                        try{
+                            int length = Integer.parseInt(lengthString);
+                            int breadth = Integer.parseInt(breadthString);
+                            if(length>0 && breadth>0){
+                                panel.addShape(new Shape(length, breadth, w/2, h/3, 159, 69, 219,"newroom"));
+                                panel.addedShape();
+                            }else{
+                                 JOptionPane.showMessageDialog(f, "Enter positive values for length and breadth.", "Invalid Input", JOptionPane.ERROR_MESSAGE);
+                            }
+                        }catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(f, "Please enter valid numeric values.", "Invalid Input", JOptionPane.ERROR_MESSAGE);
+                    }
+                    }
+                    
 				}
 		});
         panel.setLayout(null);
